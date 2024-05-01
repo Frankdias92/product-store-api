e5c99eceac9e5716cfb34681f1ba04f1f4532b57
Franklin Macedo Dias <frankmcdias@gmail.com>
2024-05-01 12:12:28 -0300

    feat: list products with tags on store

--- src/controllers/AllProductsController.js
+++ src/controllers/AllProductsController.js
@@ -2,10 +2,31 @@ const knex = require('../database/knex')
 
 class AllProductsController {
     async index(req, res) {
-        const response = await knex('produtos')
+        // const response = await knex('produtos')
         
-        console.log('passei por aqui', response)
-        return res.json(response)
+        // console.log('passei por aqui', response)
+        // return res.json(response)
+        
+        try {
+            const products = await knex('produtos')
+            const userTags = await knex('tags')
+            // const user_id = req.user.id
+
+            
+            const productsWithTags = products.map(product => {
+                const productTags = userTags.filter(tag => tag.product_id === product.id)
+                
+                return {
+                    ...product,
+                    tags: productTags
+                }
+            })
+                
+            return res.json(productsWithTags)
+        } catch (error) {
+            console.error('Error fetching products:', error)
+            return res.status(500).json({ error: 'Internal server error' })
+        }
     }
 }
 
