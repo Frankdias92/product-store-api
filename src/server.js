9ce7d659c9307dccc421983f4c633368d53d35ed
Franklin Macedo Dias <frankmcdias@gmail.com>
2024-05-07 16:12:27 -0300

    config: add variable local | dotenv

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 require('express-async-errors')
+require('dotenv/config')
+
 
 const migrationsRun = require('./database/sqlite/migrations')
 const uploadConfig = require('./configs/upload')
@@ -41,5 +43,5 @@ app.use(( error, req, res, next ) => {
 
 
 
-const PORT = 3333
+const PORT = process.env.PORT || 3333
 app.listen(PORT, () => console.log(`Server is running on address: http://localhost:${PORT}`))
