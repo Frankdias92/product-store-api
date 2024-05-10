82ff0f6c850adc6875984bae28de384e78d9ce68
Franklin Macedo Dias <frankmcdias@gmail.com>
2024-05-10 10:16:06 -0300

    test: add describe on services user and test users created

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -1,18 +1,43 @@
 const UserRepositoryInMemory = require('../repositories/UserRepositoryInMemory')
+const AppError = require('../utils/AppError')
 const UserCreateService = require('./UserCreateService')
 
-it ('user should be create', async () => {
-    const user = {
-        name: 'User Test',
-        email: 'user@test.com',
-        password: '123'
-    }
-
-    const userRepositoryInMemory = new UserRepositoryInMemory()
-    const userCreateService = new UserCreateService(userRepositoryInMemory)
-    const userCreated = await userCreateService.execute(user)
-
-    // console.log(userCreated)
+describe('UserCreateService', () => {
+    it ('user should be create', async () => {
+        const user = {
+            name: 'User Test',
+            email: 'user@test.com',
+            password: '123'
+        }
     
-    expect(userCreated).toHaveProperty('id')
+        const userRepositoryInMemory = new UserRepositoryInMemory()
+        const userCreateService = new UserCreateService(userRepositoryInMemory)
+        const userCreated = await userCreateService.execute(user)
+    
+        // console.log(userCreated)
+        
+        expect(userCreated).toHaveProperty('id')
+    })
+
+    it ('user not should be create with exist email', async () => {
+        const user1 = {
+            name: 'User1',
+            email: 'user@test.com',
+            password: '123'
+        }
+
+        const user2 = {
+            name: 'User2',
+            email: 'user@test.com',
+            password: '321'
+        }
+
+        const userRepository = new UserRepositoryInMemory()
+        const userCreateService = new UserCreateService(userRepository)
+
+        await userCreateService.execute(user1)
+        await expect(userCreateService.execute(user2)).rejects.toEqual(new AppError('Este e-mail já está em uso.'))
+
+    })
+
 })
