12f27d54a5c00c39d05ec9cdddf6ba26fdc79f68
Franklin Macedo Dias <frankmcdias@gmail.com>
2024-05-10 10:24:27 -0300

    test: add beforeEach on services.

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -3,6 +3,14 @@ const AppError = require('../utils/AppError')
 const UserCreateService = require('./UserCreateService')
 
 describe('UserCreateService', () => {
+    let userRepositoryInMemory = null
+    let userCreateService = null
+
+    beforeEach(() => {
+        userRepositoryInMemory = new UserRepositoryInMemory()
+        userCreateService = new UserCreateService(userRepositoryInMemory)
+    })
+    
     it ('user should be create', async () => {
         const user = {
             name: 'User Test',
@@ -10,8 +18,6 @@ describe('UserCreateService', () => {
             password: '123'
         }
     
-        const userRepositoryInMemory = new UserRepositoryInMemory()
-        const userCreateService = new UserCreateService(userRepositoryInMemory)
         const userCreated = await userCreateService.execute(user)
     
         // console.log(userCreated)
@@ -31,9 +37,6 @@ describe('UserCreateService', () => {
             email: 'user@test.com',
             password: '321'
         }
-
-        const userRepository = new UserRepositoryInMemory()
-        const userCreateService = new UserCreateService(userRepository)
 
         await userCreateService.execute(user1)
         await expect(userCreateService.execute(user2)).rejects.toEqual(new AppError('Este e-mail já está em uso.'))
